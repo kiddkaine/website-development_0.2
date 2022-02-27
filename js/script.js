895e10e588fa671cc892287618a1a0abbc57bc7c
Kidd Kaine <79826408+kiddkaine@users.noreply.github.com>
2022-02-27 11:39:36 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,22 +1,25 @@
 // Script slider
-
 var slideIndex = 1;
 showSlides(slideIndex);
+
 function plusSlide() {
     showSlides(slideIndex += 1);
 }
+
 function minusSlide() {
-    showSlides(slideIndex -= 1);  
+    showSlides(slideIndex -= 1);
 }
+
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
+
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("item");
     var dots = document.getElementsByClassName("slider-dots_item");
     if (n > slides.length) {
-      slideIndex = 1
+        slideIndex = 1
     }
     if (n < 1) {
         slideIndex = slides.length
